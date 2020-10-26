1a47d5bf765d98ec780dd17b886dbb1590f76769
Hsuan414 <kuoa414@gmail.com>
2020-10-25 21:24:03 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,12 +8,25 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rd2d;
     public float speed;
     public Text score;
+    public Text winText;
+    public Text livesText;
     private int scoreValue = 0;
+    private int lives;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Score: " + scoreValue.ToString();
+        SetScore();
+        lives = 3;
+        SetLivesText();
+        winText.text ="";
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
     }
     // Update is called once per frame
     void Update()
@@ -36,7 +49,14 @@ public class PlayerScript : MonoBehaviour
         if(collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
+            SetScore();
+            Destroy(collision.collider.gameObject);
+        }
+        else if(collision.collider.tag == "Enemy")
+        {
+            lives = lives - 1;
+            SetLivesText();
             Destroy(collision.collider.gameObject);
         }
     }
@@ -49,6 +69,34 @@ public class PlayerScript : MonoBehaviour
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
+        }
+    }
+
+    void SetScore()
+    {
+        if (scoreValue == 4)
+        {
+            transform.position = new Vector3(30.0f, 0.0f, 0.0f);
+            lives = 3;
+            SetLivesText();
+        }
+        if(scoreValue >= 8)
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            musicSource.loop = false;
+            Destroy(this);
+            winText.text = "You win! Game by Anita Kuo.";
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if(lives == 0)
+        {
+            Destroy(this);
+            winText.text = "You lost!";
         }
     }
 }
